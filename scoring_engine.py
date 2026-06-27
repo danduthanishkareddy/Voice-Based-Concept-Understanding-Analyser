d242921b83c49845cf365dcd015b754cffb05ca7
Dilee <thanishkareddy777@gmail.com>
2026-06-27 19:06:11 +0530

    Clean repository structure

--- scoring_engine.py
+++
@@ -1,7 +0,0 @@
-def evaluate_understanding(similarity_score):
-    if similarity_score >= 75:
-        return "Strong Understanding", "green"
-    elif similarity_score >= 50:
-        return "Moderate Understanding", "orange"
-    else:
-        return "Poor Understanding", "red"
--- speech_to_text.py
+++
@@ -1,7 +0,0 @@
-import whisper
-
-model = whisper.load_model("base")
-
-def speech_to_text(audio_path):
-    result = model.transcribe(audio_path)
-    return result["text"]
